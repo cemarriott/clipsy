e90fc8a9367d9f5598dba85c16480ed6de210d47
lapasma <lapasma@mtu.edu>
2014-02-23 23:23:42 -0500

    Luke - Lots
    Made orientations work and made new button be dialog stuff

--- Clipsy/gen/com/tsp/clipsy/R.java
+++ Clipsy/gen/com/tsp/clipsy/R.java
@@ -21,17 +21,22 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int clipsy_splash=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int b_camera=0x7f020000;
+        public static final int clipsy_splash=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int bCamera=0x7f080002;
         public static final int bEditVid=0x7f080001;
         public static final int bNewVid=0x7f080000;
+        public static final int bt_alertdialog=0x7f080004;
+        public static final int textView1=0x7f080003;
     }
     public static final class layout {
         public static final int main_menu=0x7f030000;
-        public static final int splash=0x7f030001;
+        public static final int new_project=0x7f030001;
+        public static final int splash=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
