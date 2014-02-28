ab32b82c5a2b09e7987bdd52c662e2fb26d3307f
Clayton Marriott <clayton.marriott@gmail.com>
2014-02-27 23:45:54 -0500

    Add Luke's modified files

--- Clipsy/src/com/tsp/clipsy/MainMenu.java
+++ Clipsy/src/com/tsp/clipsy/MainMenu.java
@@ -1,20 +1,11 @@
 package com.tsp.clipsy;
 
 import android.os.Bundle;
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.DialogFragment;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.Toast;
 
 public class MainMenu extends Activity {
 
@@ -28,36 +19,14 @@ public class MainMenu extends Activity {
         bnewVid.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
                 
-				//Initialising alert dialog
-                AlertDialog myAlertDialog = new AlertDialog.Builder(MainMenu.this).create();
-                //Adding the Title
-                myAlertDialog.setTitle("New Project");
-                //Adding Message
-                myAlertDialog.setMessage("Text thing here");
-                //adding an image
-                myAlertDialog.setIcon(R.drawable.ic_launcher);
-                //setting up OK button
-                myAlertDialog.setButton("OK", new DialogInterface.OnClickListener() {
- 
-                    public void onClick(DialogInterface dialog, int which) {
-                        //toast message hown after successful execution of the alert dialog
-                        Toast.makeText(getApplicationContext(), "Making Project", Toast.LENGTH_SHORT).show();
-                        
-                        // TESTING THE FILE CHOOSER
-                        
-                        Intent openFileChooser = new Intent(MainMenu.this, FileChooser.class);
-    					startActivity(openFileChooser);
- 
-                    }
-                });
- 
-                //displaying the Alert message
-                myAlertDialog.show();
- 
-           
-            	
-            }
+				Intent chooseFile = new Intent(v.getContext(), com.tsp.clipsy.FileChooser.class);
+				startActivity(chooseFile);
+				
+				
+			}
         });
+	
+        
         
         final Button beditVid = (Button) findViewById(R.id.bEditVid);
         beditVid.setOnClickListener(new View.OnClickListener() {
@@ -74,7 +43,5 @@ public class MainMenu extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
-
 
 }
