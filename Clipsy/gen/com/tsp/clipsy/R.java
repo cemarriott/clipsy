2fa43bd0f8fdb5b5647d92ebacb9eb0344fc93c2
Clayton Marriott <clayton.marriott@gmail.com>
2014-02-27 23:36:25 -0500

    Added viewer, connected FileChooser

--- Clipsy/gen/com/tsp/clipsy/R.java
+++ Clipsy/gen/com/tsp/clipsy/R.java
@@ -37,16 +37,20 @@ public final class R {
         public static final int fc_selectVideo=0x7f080004;
         public static final int fc_videoName=0x7f080000;
         public static final int fc_videoPath=0x7f080002;
+        public static final int playPause=0x7f080010;
+        public static final int seekBar=0x7f08000f;
         public static final int textView1=0x7f080003;
         public static final int textView2=0x7f080001;
         public static final int textView3=0x7f080005;
         public static final int textView5=0x7f080007;
+        public static final int videoView1=0x7f080011;
     }
     public static final class layout {
         public static final int file_chooser=0x7f030000;
         public static final int main_menu=0x7f030001;
         public static final int new_project=0x7f030002;
         public static final int splash=0x7f030003;
+        public static final int viewer=0x7f030004;
     }
     public static final class menu {
         public static final int file_chooser=0x7f070000;
@@ -56,7 +60,9 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int play_pause=0x7f050004;
         public static final int title_activity_file_chooser=0x7f050003;
+        public static final int title_activity_viewer=0x7f050005;
     }
     public static final class style {
         /** 
---
+++ Clipsy/src/com/tsp/clipsy/Viewer.java
@@ -0,0 +1,137 @@
+package com.tsp.clipsy;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.VideoView;
+
+public class Viewer extends Activity implements SeekBar.OnSeekBarChangeListener {
+	
+	private MediaPlayer mPlayer;
+	private SeekBar mSeek;
+	private Handler mHandler = new Handler();
+	private int songLength;
+	private final int SEEK_MAX = 1000;
+	
+	private String audioPath;
+	private String videoPath;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.viewer);
+		
+		
+		Bundle view = getIntent().getExtras();
+		audioPath = view.getString("audio");
+		videoPath = view.getString("video");
+		
+		
+		
+		// Video portion
+		VideoView videoView = (VideoView) findViewById(R.id.videoView1);
+        videoView.setVideoPath(videoPath);
+        
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+		
+		
+		// Audio portion
+        mSeek = (SeekBar) findViewById(R.id.seekBar);
+		mPlayer = new MediaPlayer();
+		prepareSong();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+	
+	private void prepareSong() {
+		mPlayer.reset();
+		
+		try {
+			mPlayer.setDataSource(audioPath);
+			mPlayer.prepare();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		songLength = mPlayer.getDuration();
+		
+		mSeek.setMax(SEEK_MAX); // set the seekbar length to song length
+		mSeek.setProgress(0); // restart the seekbar
+		
+		updateSeekBar();
+	}
+ 
+	public void playPause(View view) {
+
+		if (mPlayer.isPlaying()) {
+			mPlayer.pause();
+			//mHandler.removeCallbacks(mUpdateSeekBar);
+		}
+		else {
+			mPlayer.start();
+			//updateSeekBar();
+		}
+			
+	}
+	
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+        // remove message Handler from updating progress bar
+        mHandler.removeCallbacks(mUpdateSeekBar);
+    }
+	
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+        mHandler.removeCallbacks(mUpdateSeekBar);
+        /*
+        mHandler.removeCallbacks(mUpdateSeekBar);
+        int currentProgress = mSeek.getProgress();
+ 
+        // forward or backward to certain seconds
+        mPlayer.seekTo(currentProgress);
+ 
+        // update timer progress again
+        updateSeekBar();
+        */
+    }
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		// not used
+	}
+	
+	public void updateSeekBar() {
+		mHandler.postDelayed(mUpdateSeekBar, 100);
+	}
+	
+	private Runnable mUpdateSeekBar = new Runnable() {
+		public void run() {
+			mSeek.setProgress( (mPlayer.getCurrentPosition()*SEEK_MAX)/songLength );
+			mHandler.postDelayed(this, 100);
+		}
+	};
+
+}
